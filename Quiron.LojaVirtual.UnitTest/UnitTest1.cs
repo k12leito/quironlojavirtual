44bca8af558f91eaf40433a68ccdeb691061a91a
k12leito <carlos@chmsolucoes.com.br>
2016-07-17 13:17:09 -0300

    Vitrine

--- Quiron.LojaVirtual.UnitTest/UnitTest1.cs
+++ Quiron.LojaVirtual.UnitTest/UnitTest1.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using System.Collections.Generic;
 namespace Quiron.LojaVirtual.UnitTest
 {
     [TestClass]
     public class Quiron
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Take()
         {
+            //int[] numeros = {5,4,1,3,9,8,6,7,2,0};
+
+            //var resultado = from num in numeros.Take(5) select num;
+
+            //int[] teste = {5,4,1,3,9};
+
+            //CollectionAssert.AreEqual(resultado.ToArray, teste);
+
         }
     }
 }
